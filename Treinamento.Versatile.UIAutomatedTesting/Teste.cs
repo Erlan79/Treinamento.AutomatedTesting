b0267d1dbacfc7d687b19511f687e9f915ae17f8
Erlã Lima <erlanlimasantos@hotmail.com>
2022-06-20 16:32:24 -0300

    Atualizando meu projeto

--- Treinamento.Versatile.UIAutomatedTesting/Teste.cs
+++ Treinamento.Versatile.UIAutomatedTesting/Teste.cs
@@ -1,46 +1,116 @@
 ﻿
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace Treinamento.Versatile.UIAutomatedTesting
 {
     public class Teste
     {
-         // Variável do tipo CHAR
-        char InicialNomeDoMeio = 'A';
-        object CharObject = 'A';
 
-        // Variável do tipo STRING (Coleção de caracteres)
-        string Nome = "Boladão";
-        object StringObj = "String Obj";
+        #region classes, métodos, variáveis e operações lógicas
+        /*
+        public string Nome = "Boladão";
+        public int Idade = 32;
+        public bool eHomem = true;
 
-        // Variável do tipo INT
-        int Idade = 32;
-        object IntObj = 1;
+        public string RetornaStringNome(string nome) // Método 1
+        {
+            if (string.IsNullOrEmpty(nome))
+            {
+                return "O nome não foi informado";
+            }
+            else if (!string.IsNullOrEmpty(nome) && nome.Equals("Boladão"))
+            {
+                return "Boladão é o seu nome";
+            }
+            else if (!string.IsNullOrEmpty(nome) && nome.Equals("Jão") || nome.Equals("João"))
+            {
+                return "João é o seu nome";
+            }
+            else
+            {
+                return "Nome informado é diferente de Boladão e João";
+            }
 
-        // variável do tipo Boolean
-        bool EHomem = true;
-        object boolObj = false;
+        } // FIM DO MÉTODO 1
 
-  
-        [Test]
+        public string RetornaStringGenero(bool genero) // Método 2
+        {
+            if (genero == true)
+            {
+                return "O genero é Masculino";
+            }
+            else
+            {
+                return "O genero é Feminino";
+            }
+
+        } // FIM DO MÉTODO 2
+
+
+        [Test] // MÉTODO DE TESTE
         public void Test1()
         {
-            // As variáveis do tipo 'var' e 'object' são variáveis coringa que podem receber qualquer tipo de valor
-            string nomeString = "Boladão String";
-            var nomeVar = "Boladão Var";
-            object nomeObj = "Boladão Object";
+            string recebeNome = RetornaStringNome(Nome);
+            string recebeGenero = RetornaStringGenero(eHomem);
 
-            int numeroInt = 1;
-            var numeroVar = 2;
-            object numeroObj = 3;
+            Console.WriteLine(recebeNome);
+            Console.WriteLine(recebeGenero);
+        } // FIM DO MÉTODO DE TESTE
 
-            bool eHomem = true;
-            var eMulher = false;
-            object eObj = true;
+        */
+        #endregion
 
-            Console.WriteLine($"Olá! meu nome é {Nome} e tenho {Idade} anos! Sou homem {EHomem}");
-        }
 
-    }
-}
+        [Test] // MÉTODO DE TESTE
+        public void Test1()
+        {
+            int index = 0;
+
+            // 1º Exemplo é com array
+
+            string[] nomes = { "Boladão", "João", "Maria", "Pablo" };
+            while(index < nomes.Length)
+            {
+                Console.WriteLine("Imprimindo nomes pelo WHILE: " + nomes[index]);
+                index++;
+            }
+
+            for (var i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine("Imprimindo nomes pelo FOR: " + nomes[i]);
+            }
+
+            foreach (var nome in nomes)
+            {
+                Console.WriteLine("Imprimindo nomes pelo FOREACH: " + nome);
+            }
+
+            // 2º Exemplo é com List
+            List<int> list = new List<int>();
+
+            index = 1;
+
+
+            while (index <=10)
+            {
+                list.Add(index);
+                Console.WriteLine($"Adicionando o número inteiro: '{index}' na list");
+                index++;
+            }
+
+            for (var i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine("Imprimindo o número da list pelo FOR: " + list[i]);
+            }
+
+            foreach (var numero in list)
+            {
+                Console.WriteLine("Imprimindo os números inteiros da list pelo FOREACH: " + numero);
+            }
+
+        } // FIM DO MÉTODO DE TESTE
+
+    } // FIM DA CLASSE
+} // FIM DA NAMESPACE
